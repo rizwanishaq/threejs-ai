09ac8ebd3945f52d07052b5800be8060323d75c3
rizwanishaq <rizwanishaq@gmail.com>
2023-03-31 18:48:54 +0200

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import CustomButton from "./CustomButton";
+import ColorPicker from "./ColorPicker";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
+import AIPicker from "./AIPicker";
+
+export { CustomButton, ColorPicker, FilePicker, Tab, AIPicker };
